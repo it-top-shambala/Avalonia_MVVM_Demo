589639074aa65d9a9de2d78e9bc6f3a7a1b4e773
Starinin Andrey <starininandrey@gmail.com>
2022-08-17 20:45:07 +0300

    Update

--- Avalonia_MVVM_Demo.App/ViewModels/MainWindowViewModel.cs
+++ Avalonia_MVVM_Demo.App/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace Avalonia_MVVM_Demo.App.ViewModels
+﻿using System.Collections.ObjectModel;
+using Avalonia_MVVM_Demo.App.Lib;
+using Avalonia_MVVM_Demo.App.Models;
+
+namespace Avalonia_MVVM_Demo.App.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public ObservableCollection<Person> Persons { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Persons = new ObservableCollection<Person>(WebLib.GetPersons());
+        }
     }
 }
