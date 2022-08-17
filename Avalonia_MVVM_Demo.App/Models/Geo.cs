ef10a39b0977d3aec8d47c41093aa168f5dbbf7e
Starinin Andrey <starininandrey@gmail.com>
2022-08-17 20:23:34 +0300

    Add models

---
+++ Avalonia_MVVM_Demo.App/Models/Geo.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Avalonia_MVVM_Demo.App.Models;
+
+public class Geo
+{
+    [JsonPropertyName("lat")]
+    public string? Lat { get; set; }
+
+    [JsonPropertyName("lng")]
+    public string? Lng { get; set; }
+}
---
+++ Avalonia_MVVM_Demo.App/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Avalonia_MVVM_Demo.App.Models;
+
+public class Person
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("username")]
+    public string? Username { get; set; }
+
+    [JsonPropertyName("email")]
+    public string? Email { get; set; }
+
+    [JsonPropertyName("address")]
+    public Address? Address { get; set; }
+
+    [JsonPropertyName("phone")]
+    public string? Phone { get; set; }
+
+    [JsonPropertyName("website")]
+    public string? Website { get; set; }
+
+    [JsonPropertyName("company")]
+    public Company? Company { get; set; }
+}
